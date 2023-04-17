a66c047879aa02aa193c25b0ca9ebd20fc972d35
Daniel Fontanes <danielfontanesamoedo@gmail.com>
2023-04-17 22:58:15 +0200

    [IMP] UserCompany component done

---
+++ src/components/user/UserCompany.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="user-company-info">
+            <p><span>Nombre de la compañía: </span>{{ user.company.name }}</p>
+            <p><span>Departamento: </span>{{ user.company.department }}</p>
+            <p><span>Título: </span>{{ user.company.title }}</p>
+            <p><span>Dirección: </span>{{ user.company.address.address }}</p>
+            <p><span>Ciudad: </span>{{ user.company.address.city }}</p>
+            <p><span>Latitud: </span>{{ user.company.address.coordinates.lat }}</p>
+            <p><span>Longitud: </span>{{ user.company.address.coordinates.lng }}</p>
+            <p><span>Código postal: </span>{{ user.company.address.postalCode }}</p>
+            <p><span>Estado: </span>{{ user.company.address.state }}</p>
+          </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: "UserCompany",
+  computed: {
+    ...mapState(['user']),
+  },
+}
+</script>
+
+<style scoped>
+span {
+  font-weight: bold;
+}
+</style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -10,22 +10,11 @@
       </div>
 
       <div class="d-flex gap-4">
-        <UserProfile/>
+        <UserProfile class="card"/>
         <div class="d-flex flex-column gap-4">
-          <UserAdress />
-          <UserBank />
-          <UserCompany />
-          <div class="user-company-info">
-            <p><span>Nombre de la compañía: </span>{{ user.company.name }}</p>
-            <p><span>Departamento: </span>{{ user.company.department }}</p>
-            <p><span>Título: </span>{{ user.company.title }}</p>
-            <p><span>Dirección: </span>{{ user.company.address.address }}</p>
-            <p><span>Ciudad: </span>{{ user.company.address.city }}</p>
-            <p><span>Latitud: </span>{{ user.company.address.coordinates.lat }}</p>
-            <p><span>Longitud: </span>{{ user.company.address.coordinates.lng }}</p>
-            <p><span>Código postal: </span>{{ user.company.address.postalCode }}</p>
-            <p><span>Estado: </span>{{ user.company.address.state }}</p>
-          </div>
+          <UserAdress class="card"/>
+          <UserBank class="card"/>
+          <UserCompany class="card"/>
         </div>
       </div>
 
@@ -43,6 +32,7 @@
 import UserProfile from '@/components/user/UserProfile.vue';
 import UserAdress from '@/components/user/UserAdress.vue';
 import UserBank from '@/components/user/UserBank.vue';
+import UserCompany from '@/components/user/UserCompany.vue';
 
 import { mapState } from 'vuex';
 
@@ -51,7 +41,8 @@ export default {
   components: {
     UserProfile,
     UserAdress,
-    UserBank
+    UserBank,
+    UserCompany
   },
   data() {
     return {
@@ -99,25 +90,14 @@ export default {
 }
 </script>
 <style scoped>
-.quote,
-.user-primary-info,
-.user-adress-info,
-.user-bank-info,
-.user-company-info,
-.user-recomended-categories {
+.card {
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
   border-radius: 20px;
   padding: 2rem;
 }
 
-svg {
-  width: 2rem;
-}
-
 blockquote {
   font-style: italic;
-  background-color: #f9f9f9;
-  border-left: 5px solid #ccc;
   padding: 10px;
 }
 </style>
